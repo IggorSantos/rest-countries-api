352388d8f22e47cabbcb6db31a0e90bc0217f0a4
Igor Santos <iggor.jsantos@gmail.com>
2023-04-24 22:27:51 -0300

    feat(Home): inclusão do componente de card

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CardComponent } from 'src/app/components/card/card.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent,CardComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule    
   ]
 })
 export class HomeModule { }
