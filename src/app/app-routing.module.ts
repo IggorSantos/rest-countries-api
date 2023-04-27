dc3e75705f2099be9ebcb81765c521520d991a54
Igor Santos <iggor.jsantos@gmail.com>
2023-04-26 22:22:46 -0300

    feat(country-page): inclusão da pagina de país

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 const routes: Routes = [
-  {path: '', loadChildren:() => import('./pages/home/home.module').then(m => m.HomeModule) }
+  {path: '', loadChildren:() => import('./pages/home/home.module').then(m => m.HomeModule) },
+  {path: 'country-page', loadChildren:() => import('./pages/country-page/country-page.module').then(m => m.CountryPageModule) }
 ];
 
 @NgModule({
---
+++ src/app/pages/country-page/country-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CountryPageRoutingModule } from './country-page-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CountryPageRoutingModule
+  ]
+})
+export class CountryPageModule { }
