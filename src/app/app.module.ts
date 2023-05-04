a9e5d9f7a2dafa43f846332bf8d361c79b619476
Igor Santos <iggor.jsantos@gmail.com>
2023-05-03 21:52:06 -0300

    feat(search): criação da pagina de pesquisa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './pages/search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
