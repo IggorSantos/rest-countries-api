dd671b458db89807b7b7142f4a9a0d32be14d942
Igor Santos <iggor.jsantos@gmail.com>
2023-05-04 22:15:47 -0300

    feat(search): mostrando resultados da pesquisa

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HomeServiceService } from 'src/app/services/home-service.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  search: string = '';
+  countries: any;
 
-  constructor() { }
+  constructor(private activated: ActivatedRoute, private service: HomeServiceService) { }
 
   ngOnInit(): void {
+    this.search = this.activated.snapshot.params['search']
+    this.getCountries()
+    console.log(this.search)
+    console.log(this.search.replace(/['"]+/g, ''));
+  }
+
+  getCountries(){
+    this.service.getSearchCountry(this.search.replace(/['"]+/g, '')).subscribe({
+      next: (res: any) => {
+        console.log(res)
+        this.countries = res
+      },
+      error: (err: any) => {
+        console.error(err)
+      }
+    })
   }
 
 }
